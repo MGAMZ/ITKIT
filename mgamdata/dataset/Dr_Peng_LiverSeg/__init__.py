1cd06818b05df43d6291f54563935b7fa5da374e
mgam <312065559@qq.com>
2025-05-29 22:27:54 +0800

    fix bug

--- mgamdata/dataset/Dr_Peng_LiverSeg/__init__.py
+++ mgamdata/dataset/Dr_Peng_LiverSeg/__init__.py
@@ -1,8 +1,9 @@
 from .mm_dataset import LiverSeg_Precrop_Npz, LiverSeg_Semi_Mha
-from .meta import CLASS_INDEX_MAP as LIVERSEG_CLASS_INDEX_MAP
+from .meta import LIVER_TUMOR_CLASS_INDEX_MAP, LIVER_CLASS_INDEX_MAP
 
 __all__ = [
     'LiverSeg_Precrop_Npz',
     'LiverSeg_Semi_Mha',
-    'LIVERSEG_CLASS_INDEX_MAP'
+    'LIVER_TUMOR_CLASS_INDEX_MAP',
+    'LIVER_CLASS_INDEX_MAP',
 ]
