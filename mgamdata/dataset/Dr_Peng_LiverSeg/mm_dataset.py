854a6188b26142a89e72dba33653fae488aec0e6
mgam <312065559@qq.com>
2025-05-29 16:35:56 +0800

    sync Dr.Peng's project.

--- mgamdata/dataset/Dr_Peng_LiverSeg/mm_dataset.py
+++ mgamdata/dataset/Dr_Peng_LiverSeg/mm_dataset.py
@@ -1,10 +1,11 @@
 from ..base import mgam_SemiSup_3D_Mha, mgam_SemiSup_Precropped_Npz
-from .meta import CLASS_INDEX_MAP
+from .meta import LIVER_CLASS_INDEX_MAP, LIVER_TUMOR_CLASS_INDEX_MAP
 
 
+# 二分类：# 背景+肝脏
 
 class LiverSeg_base:
-    METAINFO = dict(classes=list(CLASS_INDEX_MAP.keys()))
+    METAINFO = dict(classes=list(LIVER_CLASS_INDEX_MAP.keys()))
 
 
 class LiverSeg_Precrop_Npz(LiverSeg_base, mgam_SemiSup_Precropped_Npz):
@@ -12,4 +13,18 @@ class LiverSeg_Precrop_Npz(LiverSeg_base, mgam_SemiSup_Precropped_Npz):
 
 
 class LiverSeg_Semi_Mha(LiverSeg_base, mgam_SemiSup_3D_Mha):
+    pass
+
+
+# 三分类：背景+肝脏+肝肿瘤
+
+class LiverTumorSeg_base:
+    METAINFO = dict(classes=list(LIVER_TUMOR_CLASS_INDEX_MAP.keys()))
+
+
+class LiverTumorSeg_Precrop_Npz(LiverTumorSeg_base, mgam_SemiSup_Precropped_Npz):
+    pass
+
+
+class LiverTumorSeg_Semi_Mha(LiverTumorSeg_base, mgam_SemiSup_3D_Mha):
     pass
