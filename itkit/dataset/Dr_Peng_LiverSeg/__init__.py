e767118681030d6a2778ad31413674d5ce64e695
mgam <312065559@qq.com>
2025-09-06 01:40:48 +0800

    Decouple Dr. Peng's project code from this repo.

--- itkit/dataset/Dr_Peng_LiverSeg/__init__.py
+++
@@ -1,9 +0,0 @@
-from .mm_dataset import LiverSeg_Precrop_Npz, LiverSeg_Semi_Mha
-from .meta import LIVER_TUMOR_CLASS_INDEX_MAP, LIVER_CLASS_INDEX_MAP
-
-__all__ = [
-    'LiverSeg_Precrop_Npz',
-    'LiverSeg_Semi_Mha',
-    'LIVER_TUMOR_CLASS_INDEX_MAP',
-    'LIVER_CLASS_INDEX_MAP',
-]
