27eae6012814ee72793342cca06837b799a3fd4c
mgam <312065559@qq.com>
2025-12-07 02:19:52 +0800

    Fix test image dtype

--- tests/test_itk_patch.py
+++ tests/test_itk_patch.py
@@ -13,7 +13,7 @@ from itkit.process.itk_patch import PatchProcessor, parse_patch_size
 
 @pytest.fixture
 def sample_image():
-    arr = np.random.rand(10, 10, 10).astype(np.float32)
+    arr = np.random.rand(10, 10, 10).astype(np.int16)
     img = sitk.GetImageFromArray(arr)
     img.SetSpacing((1.0, 1.0, 1.0))
     img.SetOrigin((0.0, 0.0, 0.0))
