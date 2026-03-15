ca4ecd3fabc91d64a8c127efee804c551f20a1ab
mgam <312065559@qq.com>
2026-03-15 22:38:39 +0800

    [example] update example configs

--- examples/configs/0.1.AbdomenCT1K_MONAI/mgam.py
+++ examples/configs/0.1.AbdomenCT1K_MONAI/mgam.py
@@ -34,7 +34,7 @@ from itkit.mm.visualization import SegViser, BaseVisHook, LocalVisBackend
 # --------------------PARAMETERS-------------------- #
 
 # PyTorch
-debug = True
+debug = False
 use_AMP = True
 dist = False if not debug else False  # distribution
 MP_mode = "ddp"  # Literal[`"ddp", "fsdp", "deepspeed"]
